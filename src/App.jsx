01a5b60dd38cdece35732170a004726b499bb004
Ryot211 <gallarjoel@gmail.com>
2025-05-27 22:55:47 -0500

    variables-estilos-tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import {Hero} from "./components/hero"
 import { CardsContainer } from "./components/cards-container"
 function App() {
   return (
-    <>
+    <main className="font-Poppins" >
       
       <Hero/>
       <CardsContainer/>
-    </>
+    </main>
   )
 }
 
