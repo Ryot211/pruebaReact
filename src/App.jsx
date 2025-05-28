0d735211ba5ef7237ae69dfb84d456c5763812a5
Ryot211 <gallarjoel@gmail.com>
2025-05-27 23:04:05 -0500

    Definicion de hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import {Hero} from "./components/hero"
 import { CardsContainer } from "./components/cards-container"
 function App() {
   return (
-    <main className="font-Poppins" >
-      
-      <Hero/>
+    <main className="font-Poppins flex justify-center text-Grey-500" >
+      <section className="w-[296px] my-[80px]" >
+        <Hero/>
       <CardsContainer/>
+      </section>
+      
     </main>
   )
 }
--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,10 +1,10 @@
 export const Hero = () => {
   return (
-    <header> 
-      <h1>
+    <header className="text-center" > 
+      <h1 className="text-2xl font-extralight">
       Reliable, efficient delivery</h1>
-      <h2>Powered by Technology</h2>
-      <p> Our Artificial Intelligence powered tools use millions of project data points 
+      <h2 className="text-2xl font-semibold mb-[13px]">Powered by Technology</h2>
+      <p className="text-Grey-400 mb-[76px]"> Our Artificial Intelligence powered tools use millions of project data points 
   to ensure that your project is successful</p>
     </header>
   )
